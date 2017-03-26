7c43be8a5df486a5c5fd997c702ec72b4980ee17
Øyvind Hellenes <hellenes.91@gmail.com>
2017-03-26 22:59:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app" class="green-bg">
+    <img src="./assets/header.png" id="header-img">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  height: 100vw;
+}
+
+body {
+  margin: 0;
+}
+
+.yellow {
+  color: #ebc733;
+}
+
+.green {
+  color: #587636;
+}
+
+.grellow {
+  color: #8c8d30;
+}
+.beige {
+  color: #fef1cf;
+}
+
+.yellow-bg {
+  background-color: #ebc733;
+}
+
+.green-bg {
+  background-color: #587636;
+}
+
+.grellow-bg {
+  background-color: #8c8d30;
+}
+.beige-bg {
+  background-color: #fef1cf;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+#spacer {
+  height: 20px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  width: 100%;
+}
+
+#header-img {
+  width: 100%;
+  margin-bottom: 30px;
+  box-shadow: 0px 15px 30px #000000;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="home container grellow-bg">
+    <h1 class="beige">Bli med og støtt Noregs første folkefinansierte idrettsanlegg!</h1>
+    <div class="progress beige-bg">
+      <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="32" aria-valuemin="0" aria-valuemax="100" style="width: 32%">
+        <p class="lead bold green">
+          31 200kr
+        </p>
+      </div>
+    </div>
+    <div>
+      <button type="button" class="btn btn-primary btn-lg beige green-bg bold">
+        BLI MED!
+      </button>
+    </div>
+    <img src="../assets/spacer.png" id="spacer">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.progress-bar-animated {
+  background-color: #ebc733;
+  height: 40px;
+}
+.progress {
+  margin-top: 50px;
+  margin-bottom: 30px;
+}
+
+.progress p {
+  margin-top: 10px;
+}
+
+.btn-lg {
+  height: 80px;
+}
+
+.container {
+  border-width: 13px;
+  border-style: solid;
+  border-color:  #fef1cf;
+  padding: 50px;
+  margin-top: 100px;
+}
+
+</style>
